53dcd3f642e87f41a60f9ef6018554ba231e1414
noisehunter82 <69931086+noisehunter82@users.noreply.github.com>
2020-11-24 16:49:47 +0000

    Add 'this'

--- Droid.java
+++ Droid.java
@@ -3,35 +3,35 @@ public class Droid {
   String name;
   int batteryLevel;
 
-  public Droid(String droidName) {
-    name = droidName;
-    batteryLevel = 100;
+  public Droid(String name) {
+    this.name = name;
+    this.batteryLevel = 100;
   }
 
   public String toString() {
-    return "Hello, I'm the droid: " + name + "\n";
+    return "Hello, I'm the droid: " + this.name + "\n";
   }
 
   public void performTask(String task) {
-    System.out.println("Droid " + name + " is performing task: " + task + "\n");
-    batteryLevel -= 10;
+    System.out.println("Droid " + this.name + " is performing task: " + task + "\n");
+    this.batteryLevel -= 10;
   }
 
   public void stateBatteryLevel() {
-    System.out.println("Droid " + name + "'s battery level is: " + batteryLevel + "\n");
+    System.out.println("Droid " + this.name + "'s battery level is: " + this.batteryLevel + "\n");
   }
 
   public void shareEnergy(Droid otherDroid, int energyAmount) {
     // Check battery level. Droid can't transfer more than 50% of its current
     // battery level.
-    if (energyAmount <= batteryLevel / 2) {
-      batteryLevel -= energyAmount;
+    if (energyAmount <= this.batteryLevel / 2) {
+      this.batteryLevel -= energyAmount;
       otherDroid.batteryLevel += energyAmount;
       System.out.println("Energy transfer complete\n");
       stateBatteryLevel();
       otherDroid.stateBatteryLevel();
     } else {
-      System.out.println(name + "'s battery level is too low! Try transferring smaller amount of energy\n");
+      System.out.println(this.name + "'s battery level is too low! Try transferring smaller amount of energy\n");
     }
   }
 
